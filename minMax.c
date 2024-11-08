b9f77acda79173da197b1d7f83b7353cd67d06f8
Jahid Hasan <jahid.hasan.shanto@|gmail.com>
2024-11-08 12:54:00 +0600

    Find the Missing Number

--- minMax.c
+++ minMax.c
@@ -1,31 +1,30 @@
 #include <stdio.h>
 
 int main() {
-    int N, M;
-   scanf("%d %d", &N, &M);
+    int T;
+    scanf("%d", &T);
     
-    if (N != M) {
-        printf("NO\n");
-        return 0;
-    }
-    int matrix[N][M];
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            scanf("%d", &matrix[i][j]);
+    while (T--) {
+        long long M, A, B, C;
+        scanf("%lld %lld %lld %lld", &M, &A, &B, &C);
+        long long product = A * B * C;
+
+
+        if (product == 0) {
+            if (M == 0) {
+                printf("0\n");
+            } else {
+                printf("-1\n");
+            }
+        } else {
+        if (M % product == 0) {
+            long long X = M / product;
+            printf("%lld\n", X);
+        } else {
+            printf("-1\n");
+        }
         }
     }
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            if ((i == j || i + j == N - 1) && matrix[i][j] != 1) {
-                printf("NO\n");
-                return 0;
-            }
-        if (i != j && i + j != N - 1 && matrix[i][j] != 0) {
-            printf("NO\n");
-            return 0;
-            }
-        }
-    }
-    printf("YES\n");
+
     return 0;
 }
