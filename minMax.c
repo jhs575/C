95a098cd73e13920ca1a04c06258e4d0af34d8d5
Jahid Hasan <jahid.hasan.shanto@|gmail.com>
2024-11-08 12:58:35 +0600

    Swap in Matrix

--- minMax.c
+++ minMax.c
@@ -1,29 +1,27 @@
 #include <stdio.h>
 
 int main() {
-    int T;
-    scanf("%d", &T);
+    int N, M;
+    scanf("%d %d", &N, &M);
     
-    while (T--) {
-        long long M, A, B, C;
-        scanf("%lld %lld %lld %lld", &M, &A, &B, &C);
-        long long product = A * B * C;
-
-
-        if (product == 0) {
-            if (M == 0) {
-                printf("0\n");
-            } else {
-                printf("-1\n");
-            }
-        } else {
-        if (M % product == 0) {
-            long long X = M / product;
-            printf("%lld\n", X);
-        } else {
-            printf("-1\n");
+    int matrix[N][M];
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            scanf("%d", &matrix[i][j]);
         }
+    }for (int i = 0; i < N; i++) {
+        int temp = matrix[i][0];
+        matrix[i][0] = matrix[i][M-1];
+        matrix[i][M-1] = temp;
+    }for (int j = 0; j < M; j++) {
+        int temp = matrix[0][j];
+        matrix[0][j] = matrix[N-1][j];
+        matrix[N-1][j] = temp;
+    }for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            printf("%d ", matrix[i][j]);
         }
+        printf("\n");
     }
 
     return 0;
